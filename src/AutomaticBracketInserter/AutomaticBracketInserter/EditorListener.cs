5aa4e0dfc8259f89bf4cb6e788404f35ff0e3584
Mücahit İmre <mucahit.imre@proj-e.com>
2020-01-28 21:02:46 +0300

    development complete

---
+++ src/AutomaticBracketInserter/AutomaticBracketInserter/EditorListener.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace AutomaticBracketInserter
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class EditorListener : IWpfTextViewCreationListener
+    {
+        [Import] private readonly IAsyncCompletionBroker _completionBroker = null;
+        [Import] private readonly IEditorOperationsFactoryService _editorOperationsFactory = null;
+
+        private ITextView TextView { get; set; }
+        private IEditorOperations EditorOperations { get; set; }
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            if (_completionBroker != null)
+            {
+                _completionBroker.CompletionTriggered += CompletionBroker_CompletionTriggered;
+            }
+        }
+
+        private void CompletionBroker_CompletionTriggered(object sender, CompletionTriggeredEventArgs eventArgs)
+        {
+            eventArgs.CompletionSession.ItemCommitted += CompletionSession_ItemCommitted;
+
+            TextView = eventArgs.TextView;
+        }
+
+        private void CompletionSession_ItemCommitted(object sender, CompletionItemEventArgs itemEventArgs)
+        {
+            if (TryIsChooseMethod(itemEventArgs))
+            {
+                EditorOperations = _editorOperationsFactory.GetEditorOperations(TextView);
+                string wrappedSelectionText = "()";
+                EditorOperations.InsertText(wrappedSelectionText);
+            }
+        }
+
+        private bool TryIsChooseMethod(CompletionItemEventArgs itemEventArgs)
+        {
+            if (itemEventArgs.Item.Filters.IsEmpty)
+            {
+                return false;
+            }
+
+            foreach (var item in itemEventArgs.Item.Filters)
+            {
+                if (item.AccessKey == "m" && item.DisplayText == "Methods")
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
